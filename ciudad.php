87a4d403295b09ce24b172cfdcb84a8c3568563d
jonathan <jjfique@gmail.com>
2020-02-28 22:03:03 -0500

    app terminada

---
+++ ciudad.php
@@ -0,0 +1,5 @@
+<?php
+  require('./metodos.php'); //Libreria requerida para el funcionamiento.
+  $getData = leerDatos(); //Lee los datos
+  obtnciudad($getData) //Ejecuta y obtiene la ciudad
+ ?>
---
+++ tipo.php
@@ -0,0 +1,5 @@
+<?php
+  require('./metodos.php');
+  $getData = leerDatos(); //Leer la informacion del archivo json.
+  obtnTipo($getData)
+ ?>
